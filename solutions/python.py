ad0d9c94e700c6dd892c9c00334e965c037277f5
Ian G <66448861+i9nn@users.noreply.github.com>
2022-03-10 15:29:16 -0800

    Update python.py

--- solutions/python.py
+++ solutions/python.py
@@ -10,4 +10,8 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x)[::-1] == str(x)
 
+    # 1920. Build Array from Permutation [EASY] 
+    def buildArray(self, nums: List[int]) -> List[int]:
+        lst = [nums[i] for i in nums]
+        return lst
         
