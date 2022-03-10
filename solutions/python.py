09d634f39777344ef7154c570fcf16f9102ff159
Ian <iangaunt999@gmail.com>
2022-03-10 10:46:21 -0800

    Finished first python exercise

--- solutions/python.py
+++ solutions/python.py
@@ -1,1 +1,13 @@
-##
+#
+#   My Java solutions to the LeetCode Java exercises. 
+#   Feel free to poke around.
+#
+#   Exercises finished: 1
+#
+
+class Solution:
+    # 9. Palindrome Number [EASY]
+    def isPalindrome(self, x: int) -> bool:
+        return str(x)[::-1] == str(x)
+
+        
