8ad3992f84f1cbcf9e7c5b3ca608845b97e4d2b2
Ian G <66448861+i9nn@users.noreply.github.com>
2022-03-22 08:19:03 -0700

    Update python.py

--- solutions/python.py
+++ solutions/python.py
@@ -6,12 +6,22 @@
 #
 
 class Solution:
+    # 1. Two Sum [EASY]
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        solutions = ["a", "b"]
+        for i in range(len(nums)):
+                solutions = [i, nums.index(target - nums[i])] if target - nums[i] in nums and nums.index(target - nums[i]) != i else solutions
+                if solutions != ["a", "b"]:
+                    return solutions
+            
+        return solutions
+    
     # 9. Palindrome Number [EASY]
-    def isPalindrome(self, x: int) -> bool:
+    def is_palindrome(self, x: int) -> bool:
         return str(x)[::-1] == str(x)
 
     # 1920. Build Array from Permutation [EASY] 
-    def buildArray(self, nums: List[int]) -> List[int]:
+    def build_array(self, nums: List[int]) -> List[int]:
         lst = [nums[i] for i in nums]
         return lst
         
