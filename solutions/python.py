9f92347678c7e448211c0d9e98211579fddcd0aa
Ian G <66448861+i9nn@users.noreply.github.com>
2022-03-22 10:06:38 -0700

    Update python.py

--- solutions/python.py
+++ solutions/python.py
@@ -19,6 +19,23 @@ class Solution:
     # 9. Palindrome Number [EASY]
     def is_palindrome(self, x: int) -> bool:
         return str(x)[::-1] == str(x)
+    
+    # 14. Longest Common Prefix [EASY]
+    def longestCommonPrefix(self, strs: List[str]) -> str: 
+        if len(strs) == 1:
+            return strs[0]
+        
+        for i in range(len(strs[0]) + 1):
+            prefix = strs[0][:i]
+            for j in strs:
+                if not j.startswith(prefix):
+                    return prefix[:-1]
+                
+            if len(prefix) == len(strs[0]):
+                return strs[0]
+                
+
+        return ""
 
     # 1920. Build Array from Permutation [EASY] 
     def build_array(self, nums: List[int]) -> List[int]:
