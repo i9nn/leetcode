5616e40a2e5e9a8caecfd71d8769ad8febf69fc0
Ian <iangaunt999@gmail.com>
2022-04-19 08:27:03 -0700

    Reformatted top comment

--- solutions/cplusplus.cpp
+++ solutions/cplusplus.cpp
@@ -1,8 +1,14 @@
+/*
+    My C++ solutions to the LeetCode C++ exercises. 
+    Feel free to poke around.
+
+    Exercises finished: 1
+*/
 
 // 2235. Add Two Integers [EASY]
 class Solution {
-public:
-    int sum(int num1, int num2) {
-        return num1 + num2; 
-    }
+    public:
+        int sum(int num1, int num2) {
+            return num1 + num2; 
+        }
 };
